9528eaff7ad5f3d6f9e4034d6984e4ec9301e68d
RobWiederstein <khuon68@gmail.com>
2022-12-10 18:41:08 -0500

    run --> preview, no render

--- R/viewArticle.R
+++ R/viewArticle.R
@@ -5,5 +5,16 @@
 #' @export
 #'
 viewArticle <- function(){
-    quarto::quarto_run(input = system.file("article", "index.qmd", package = "helloQuarto"))
+    # articleDir <- system.file("article", package = "helloQuarto")
+    # setwd(articleDir)
+    # quarto::quarto_run(input = "index.qmd")
+    quarto::quarto_preview(
+        file = system.file("article", package = "helloQuarto"),
+        render = FALSE,
+        port = "auto",
+        host = "127.0.0.1",
+        browse = TRUE,
+        watch = TRUE,
+        navigate = TRUE
+    )
 }
